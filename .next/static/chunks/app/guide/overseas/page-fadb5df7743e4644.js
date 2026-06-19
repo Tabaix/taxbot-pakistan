64b2c9b3077286f1eaea0fabaf6ab228c6d432ad
Tayyab Ali <tayyabraza786mughal@gmail.com>
2026-06-19 16:36:55 +0300

    Fix .gitignore: exclude .next build folder

--- .next/static/chunks/app/guide/overseas/page-fadb5df7743e4644.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[80,701,284,931,871],{8026:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=8026)}),_N_E=n.O()}]);
